e387c941008dcea5a7a3634a9179e0760041467e
metaf <o.ryabenko1@gmail.com>
2026-02-04 13:36:44 +0300

    добавлена возможность передать content-type в спеку, рефакторинг

--- src/main/java/api/specs/RequestSpecs.java
+++ src/main/java/api/specs/RequestSpecs.java
@@ -17,36 +17,27 @@ public class RequestSpecs {
 
     private static RequestSpecBuilder defaultRequestBuilder() {
         return new RequestSpecBuilder()
-                .setContentType(ContentType.JSON)
-                .setAccept(ContentType.JSON)
                 .addFilters(
                         List.of(new RequestLoggingFilter(),
                                 new ResponseLoggingFilter()))
-                .setBaseUri(Config.getProperty("BaseUrl")+Config.getProperty("api"));
+                .setBaseUri(Config.getProperty("BaseUrl") + Config.getProperty("api"));
     }
 
-    public static RequestSpecification adminSpec() {
+    public static RequestSpecification authAsUser(String username, String password, ContentType type) {
         return defaultRequestBuilder()
-                .addHeader("Authorization", "Basic " +
-                        Base64.getEncoder().encodeToString(
-                                (Config.getProperty("admin.login") + ":" + Config.getProperty("admin.password"))
-                                        .getBytes()))
-                .build();
-    }
-
-    public static RequestSpecification authAsUser(String username, String password) {
-        return defaultRequestBuilder()
+                .setContentType(type)
+                .setAccept(type)
                 .addHeader("Authorization", "Basic " +
                         Base64.getEncoder().encodeToString((username + ":" + password)
                                 .getBytes()))
                 .build();
     }
 
-    /*public static RequestSpecification base() {
-        return given()
-                .baseUri(Config.getProperty("apiBaseUrl"))
-                .header("Authorization", "Bearer " + Config.getProperty("token"))
-                .header("Accept", "application/json")
-                .header("Content-Type", "application/json");
-    }*/
+    public static RequestSpecification authAsUser(String username, String password) {
+        return authAsUser(username, password, ContentType.JSON);
+    }
+
+    public static RequestSpecification adminSpec() {
+        return authAsUser(Config.getProperty("admin.login"), Config.getProperty("admin.password"));
+    }
 }
