47d1891d5c6ab46622f99842b0bc10caf3441b66
seva931 <196225365+seva931@users.noreply.github.com>
2026-02-06 19:57:48 +0300

    1)В ParentProject и ProjectResponse добавлена аннотация @JsonIgnoreProperties(ignoreUnknown = true) для корректной десериализации ответов 2)В ProjectManagementSteps переписан updateProjectName под текущую реализацию put 3)В RequestSpecs удалён избыточный метод adminSpec(ContentType type) 4)В ResponseSpecs добавлены спецификации для статусов 400 Bad Request и 401 Unauthorized 5)Создал пакет api.projects, добавлены тесты: ProjectCreateTest (создание проекта, включая негатив на дубль id) и ProjectGetListTest (получение списка проектов)

--- src/main/java/api/specs/RequestSpecs.java
+++ src/main/java/api/specs/RequestSpecs.java
@@ -61,11 +61,4 @@ public class RequestSpecs {
                                 .getBytes()))
                 .build();
     }
-    public static RequestSpecification adminSpec(ContentType type) {
-        return authAsUser(
-                Config.getProperty("admin.login"),
-                Config.getProperty("admin.password"),
-                type
-        );
-    }
 }
