a36a2572bd1b1abd435331762c4576c2b7acd20a
metaf <o.ryabenko1@gmail.com>
2026-02-07 11:14:51 +0300

    модель ответа с ошибками

---
+++ src/main/java/api/models/ErrorDetail.java
@@ -0,0 +1,19 @@
+package api.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetail {
+    private String stackTrace;
+    private String message;
+    private String statusText;
+    private String additionalMessage;
+}
