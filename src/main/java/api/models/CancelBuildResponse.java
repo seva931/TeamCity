62ed7cc51890cb6e9914928ba6c91d858a45f80b
metaf <o.ryabenko1@gmail.com>
2026-02-20 19:03:07 +0300

    Build Queue тесты

---
+++ src/main/java/api/models/CancelBuildResponse.java
@@ -0,0 +1,21 @@
+package api.models;
+
+public class CancelBuildResponse {
+    private long id;
+    private String buildTypeId;
+    private String status;
+    private String state;
+    private String statusText;
+
+
+
+//        "id": 666,
+//        "buildTypeId": "XfGBHFPFOOsL",
+//        "number": "1",
+//        "status": "UNKNOWN",
+//        "state": "running",
+//        "percentageComplete": -1,
+//        "href": "/app/rest/builds/id:666",
+//        "webUrl": "http://localhost:8111/buildConfiguration/XfGBHFPFOOsL/666",
+//        "statusText": "Canceled",
+}
--- src/main/java/api/requests/steps/BuildQueueSteps.java
+++ src/main/java/api/requests/steps/BuildQueueSteps.java
@@ -3,20 +3,14 @@ package api.requests.steps;
 import api.models.*;
 import api.requests.skeleton.Endpoint;
 import api.requests.skeleton.requesters.CrudRequester;
-import api.requests.skeleton.requesters.ValidatedCrudRequester;
 import api.specs.RequestSpecs;
 import api.specs.ResponseSpecs;
-import common.data.BuildStepPropertyData;
-import common.data.BuildStepTypeData;
-import common.generators.RandomModelGenerator;
 import io.restassured.http.ContentType;
-
-import java.util.List;
 
 public class BuildQueueSteps {
 
     public static BuildQueueResponse queueBuild(CreateBuildTypeResponse build, CreateUserResponse user) {
-        QueueBuildRequest body = QueueBuildRequest.of(build.getId());
+        BuildQueueRequest body = BuildQueueRequest.of(build.getId());
 
         return new CrudRequester(
                 RequestSpecs.authAsUser(user, ContentType.JSON),
@@ -35,29 +29,17 @@ public class BuildQueueSteps {
         ).delete(queueId);
     }
 
-    public static BuildQueueResponse getAllQueuedBuilds(CreateUserResponse user) {
-        return new ValidatedCrudRequester<BuildQueueResponse>(
+    public static BuildQueueListResponse getAllQueuedBuilds(CreateUserResponse user) {
+        return new CrudRequester(
                 RequestSpecs.authAsUser(user, ContentType.JSON),
                 Endpoint.BUILD_QUEUE,
                 ResponseSpecs.requestReturnsOk()
-        ).get();
+        ).get().extract().as(BuildQueueListResponse.class);
     }
 
     public static BuildQueueResponse queueEndlessBuild(CreateBuildTypeResponse build, CreateUserResponse user) {
 
         BuildStepsSteps.addEndlessStep(user, build);
-
-        QueueBuildRequest request = QueueBuildRequest.builder().buildType(
-                BuildTypeRef.builder()
-                        .id(build.getId()).build()
-        ).build();
-
-        return new CrudRequester(
-                RequestSpecs.authAsUser(user, ContentType.JSON),
-                Endpoint.BUILD_QUEUE,
-                ResponseSpecs.requestReturnsOk()
-        ).post(request)
-                .extract()
-                .as(BuildQueueResponse.class);
+        return BuildQueueSteps.queueBuild(build, user);
     }
 }
