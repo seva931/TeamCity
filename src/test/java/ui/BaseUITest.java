e7fd27cee855cce5f298382fa68fda4096581a8c
Anna.Politaeva <Anna.Politaeva@tatar.ru>
2026-02-20 10:17:58 +0300

    * добавила настройки

--- src/test/java/ui/BaseUITest.java
+++ src/test/java/ui/BaseUITest.java
@@ -1,13 +1,21 @@
 package ui;
 
+import api.configs.Config;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Map;
 
 public class BaseUITest {
     @BeforeAll
     public static void setupSelenide() {
+        Configuration.remote = Config.getProperty("uiRemote");
         Configuration.baseUrl = api.configs.Config.getProperty("ui.baseUrl");
         Configuration.browser = api.configs.Config.getProperty("browser");
         Configuration.browserSize = api.configs.Config.getProperty("browser.size");
+
+        Configuration.browserCapabilities.setCapability("selenoid:options",
+                Map.of("enableVNC", true, "enebleLog", true)
+        );
     }
 }
