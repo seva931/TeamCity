1265d212df2ac783abd35022d070c90f6eab174f
metaf <o.ryabenko1@gmail.com>
2026-02-21 12:59:16 +0300

    Компонент попапа

---
+++ src/main/java/ui/pages/Popup.java
@@ -0,0 +1,36 @@
+package ui.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class Popup extends BaseComponent<Popup> {
+    public Popup(SelenideElement self) {
+        super(self);
+    }
+
+    public Popup() {
+        super($("[data-test='ring-popup']"));
+    }
+
+    public Popup shouldBeOpened() {
+        self.shouldBe(visible);
+        return this;
+    }
+
+    public Popup addComment(String comment) {
+        self.$("textarea.ring-input-input").sendKeys(comment);
+        return this;
+    }
+
+    public Popup submit() {
+        self.$("button[type='submit']").click();
+        return this;
+    }
+
+    public Popup cancel() {
+        self.$("[data-test='cancel']").click();
+        return this;
+    }
+}
